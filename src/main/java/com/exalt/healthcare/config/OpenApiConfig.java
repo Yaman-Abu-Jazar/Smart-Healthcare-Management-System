42c1ab1ad6636586ef5a28a6030b3e4ac74b2df2
Yaman Abu Jazar <yamanabujazar@gmail.com>
2025-09-15 14:51:25 +0300

    editing the swagger version

--- src/main/java/com/exalt/healthcare/config/OpenApiConfig.java
+++ src/main/java/com/exalt/healthcare/config/OpenApiConfig.java
@@ -1,29 +1,24 @@
 package com.exalt.healthcare.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Healthcare Management API",
-                version = "1.0",
-                description = "API documentation for Healthcare Management System"
-        ),
-        security = {
-                @SecurityRequirement(name = "bearerAuth")
-        }
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 public class OpenApiConfig {
+    private static final String SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Clinic API").version("v1"))
+                .components(new Components().addSecuritySchemes(SCHEME_NAME,
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME));
+    }
 }
 
